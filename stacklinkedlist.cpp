aaf1fa6192b7caab2d61b404c950d6c976918137
coannanaufal-debug <coanna.naufal@gmail.com>
2026-05-15 15:08:30 +0700

    Fix switch case structure to handle invalid choices in main function

--- stacklinkedlist.cpp
+++ stacklinkedlist.cpp
@@ -95,3 +95,7 @@ switch (choice) {
         case 4:
         cout << "Exiting program..." << endl;
         break;
+        default:
+        cout << "Invalid choice. Please try again." << endl;
+        break;
+}
